bcb90010530350d25d145f6d0757278df41eddeb
Jack Dunning <JackStunning9001@gmail.com>
2020-04-03 15:47:35 -0700

    Enable CORS

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -19,14 +20,42 @@ namespace AnimalShelter
             Configuration = configuration;
         }
 
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins, builder =>
+                {
+                    builder.WithOrigins("http://localhost:*");
+                });
+
+                options.AddPolicy("AllowSubdomain", builder =>
+                {
+                    builder.WithOrigins("http://localhost:*")
+                        .SetIsOriginAllowedToAllowWildcardSubdomains();
+                });
+
+                options.AddPolicy("AllowAllHeaders", builder =>
+                {
+                    builder.WithOrigins("http://localhost:*")
+                        .AllowAnyHeader();
+                });
+
+                options.AddPolicy("AllowCredentials", builder =>
+                {
+                    builder.WithOrigins("http://localhost:*")
+                        .AllowCredentials();
+                });
+            });
             services.AddDbContext<AnimalContext>(opt =>
                 opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+                services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +71,7 @@ namespace AnimalShelter
                 app.UseHsts();
             }
 
+            app.UseCors(MyAllowSpecificOrigins);
             // app.UseHttpsRedirection();
             app.UseMvc();
         }
